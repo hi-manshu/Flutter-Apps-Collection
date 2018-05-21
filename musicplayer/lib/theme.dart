69111a44a6b4adcd3db61fcef3ce50305a3c0d78
hi_man_shoe <hello2himanshusingh@gmail.com>
2018-05-21 11:10:35 +0530

    Added ->All Apps

--- musicplayer/lib/theme.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/material.dart';
-
-const Color accentColor = const Color(0xFFB388FF);
-const Color lightAccentColor = const Color(0xFF7C4DFF);
-const Color darkAccentColor = const Color(0xFF6200EA);
